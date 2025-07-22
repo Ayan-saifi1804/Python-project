2ce9a1244408e580257985f160fc0873dfc5626d
Ayan-saifi1804 <ayansaifi1804@gmail.com>
2025-07-22 23:52:14 +0530

    adding try.py

---
+++ try.py
@@ -0,0 +1,21 @@
+# Write a program to check if a number is even or odd.
+
+# a = int(input("Enter Number: "))
+# if a % 2 == 0:
+#     print(f"{a} is Even. ")
+# else:
+#     print(f"{a} is Odd. ")
+
+
+# Write a program to find the largest among three numbers.
+
+# Program to reverse a given string
+
+# Input from the user
+text = input("Enter a string: ")
+
+# Reversing the string
+a = text[::-1]
+
+# Display the result
+print(f"The reversed string is: {a}")
