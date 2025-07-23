6ea5bca13057db160fea77a79cce1eb9a89cdf92
Ayan-saifi1804 <ayansaifi1804@gmail.com>
2025-07-23 23:55:35 +0530

    adding Q2.py,Q3.py

---
+++ Q2.py
@@ -0,0 +1,10 @@
+# Write a program to find the sum of all elements in a list.
+
+list = [10, 20, 30, 50]
+
+sum = 0
+
+for num in list:
+    sum += num
+
+print("The sum of list is:", sum)
---
+++ Q3.py
@@ -0,0 +1,9 @@
+# Write a program to find the maximum and minimum elements in a list.
+
+numbers = [2, 5, 10, 11, 35, 9, 45, 67, 87, 99]
+
+maximum = max(numbers)
+minimum = min(numbers)
+
+print("Maximum number is:", maximum)
+print("Minimum number is:", minimum)
